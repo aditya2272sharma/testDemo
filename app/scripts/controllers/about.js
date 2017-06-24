c83ce6d5e7384eb195eec74ac9e08e9b8eaee863
aditya sharma <aditya.2272.sharma@gmail.com>
2017-06-25 03:59:57 +0530

    install additional required packages

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testDemoApp
+ */
+angular.module('testDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testDemoApp
+ */
+angular.module('testDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
