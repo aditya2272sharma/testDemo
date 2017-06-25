053be4aae506fb69d1c78bef5d489f3af7b43ca7
aditya sharma <aditya.2272.sharma@gmail.com>
2017-06-25 06:08:17 +0530

    bare minimum 1.0

---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testDemoApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the testDemoApp
+ */
+angular.module('testDemoApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
 // Generated on 2017-06-24
+// Re-Generated on Sun Jun 25 2017 05:52:33 GMT+0530 (IST)
 
 module.exports = function(config) {
   'use strict';
@@ -13,6 +14,8 @@ module.exports = function(config) {
 
     // testing framework to use (jasmine/mocha/qunit/...)
     // as well as any additional frameworks (requirejs/chai/sinon/...)
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [
       'jasmine'
     ],
@@ -43,8 +46,11 @@ module.exports = function(config) {
     ],
 
     // web server port
-    port: 8080,
+    // port: 8080,
+    port: 9876,
 
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    // browsers: ['Chrome', 'PhantomJS', 'Firefox'],
     // Start these browsers, currently available:
     // - Chrome
     // - ChromeCanary
@@ -54,24 +60,50 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      'PhantomJS'
+      'PhantomJS',
+      'Firefox'
     ],
+
+    browserDisconnectTimeout: 2000,
+    browserNoActivityTimeout: 10000,
 
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-spec-reporter',
+      'karma-jasmine-html-reporter'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    
+    reporters: [
+      'spec',
+      'kjhtml'
     ],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false,
 
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
@@ -79,5 +111,9 @@ module.exports = function(config) {
     // },
     // URL root prevent conflicts with the site root
     // urlRoot: '_karma_'
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
   });
 };
